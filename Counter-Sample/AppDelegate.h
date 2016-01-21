1cb3c153f79f199b55bcaafb9861a3ce93a22637
harquail <nook@harquail.com>
2016-01-20 20:49:44 -0500

    Initial Commit

---
+++ Counter-Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Counter-Sample
+//
+//  Created by nook on 1/20/16.
+//  Copyright © 2016 nook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Counter-Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Counter-Sample
+//
+//  Created by nook on 1/20/16.
+//  Copyright © 2016 nook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
